e7c734cfb8aa5e65a8fb8bccf5fce556ec89af33
Konstantin Potekaev <Konstantin.Potekaev@infowatch.com>
2025-04-11 13:26:35 +0300

    - CW_1: Удален неудавшийся проект на питоне;

--- CW_1/docker/scripts/run_bas.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -e
-
-echo "Запуск backend (bank_account_service)..."
-docker run -d -p 8000:8000 --name bank_account_service bank_account_service
-echo "Backend запущен!"
--- CW_1/utils/bas_utils/transaction.py
+++
@@ -1,8 +0,0 @@
-from enum import Enum
-
-from bas_processor.common.const.transaction import INCOME, EXPENSE
-
-
-class TransactionType(str, Enum):
-    income = INCOME
-    expense = EXPENSE
